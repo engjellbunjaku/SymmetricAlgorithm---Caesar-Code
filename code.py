756cab9dcfaa4a0fe1c8790cc087e981d2b36e87
Engjëll Bunjaku <93830359+engjellbunjaku@users.noreply.github.com>
2022-04-16 18:04:35 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,19 @@
+from random import randint
 
+
+def encrypt(text, shift):
+    result = ""
+
+    for i in range(len(text)):  # 0 1 2 3
+        char = text[i]
+
+        if (ord(char) < 65 or ord(char) > 122) or (91 <= ord(char) <= 96):
+            result += chr(ord(char))
+        else:
+
+            if char.isupper():
+                result += chr((ord(char) + shift - 65) % 26 + 65)
+            else:
+                result += chr((ord(char) + shift - 97) % 26 + 97)
+
+    return result
